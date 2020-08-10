62a03169b15f79a0beae544b3f6e8bba92c30291
Eden <49904302+giteden@users.noreply.github.com>
2020-08-10 15:59:59 +0300

    Update use-get-jokes.ts

--- components/hooks/use-get-jokes/use-get-jokes.ts
+++ components/hooks/use-get-jokes/use-get-jokes.ts
@@ -6,7 +6,6 @@ export const useGetJokes = () : [() => Promise<void>, string[], boolean, string]
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
     const endpoint = 'https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=racist&type=single';
-    
 
     const getJoke = async () => {
           setIsLoading(true);
@@ -24,9 +23,10 @@ export const useGetJokes = () : [() => Promise<void>, string[], boolean, string]
         }      
       }
     
+    // Fetch data on first mount
       useEffect(() => {
         getJoke();
     }, [])
-
+    
     return [getJoke, joke, isLoading, error]
 }
