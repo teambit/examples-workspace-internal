5bd66c618f7b3597d65f1732ba026c90be500d6d
itaymendel <itay.mendelawy@gmail.com>
2020-08-28 00:54:29 +0300

    added some basic docs

--- components/modules/capitalize/capitalize.docs.tsx
+++ components/modules/capitalize/capitalize.docs.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { CodeSnippet } from '@teambit/documenter.ui.code-snippet';
+
+const codeString = `capitalize("my upper case letters")
+// => MY UPPER CASE LETTERS
+`;
 
 export default function Overview() {
-  return <div></div>;
+  return <div><CodeSnippet>{codeString}</CodeSnippet></div>;
 }
+
+export const labels = ['strings']
+
+export const abstract = 'Capitilize a string'
