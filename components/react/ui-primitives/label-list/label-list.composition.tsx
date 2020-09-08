14ae9f9aecf506a522437be3dd88b6b995b92ec6
itaymendel <itay.mendelawy@gmail.com>
2020-09-08 14:07:02 +0300

    added two new components

---
+++ components/react/ui-primitives/label-list/label-list.composition.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LabelList } from "./label-list";
+
+const list = [
+  'light label',
+  'light label2'
+]
+
+export const LightLabel = () => {
+  return (
+      <LabelList>{list}</LabelList>
+  );
+};
