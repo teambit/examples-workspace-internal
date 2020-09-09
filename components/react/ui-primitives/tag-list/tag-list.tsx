4511b7e7c2b9e60521a6ac4726704c58f4d07149
oded <oded@bit.dev>
2020-09-09 17:48:46 +0300

    tag list - added type description and styles

--- components/react/ui-primitives/tag-list/tag-list.tsx
+++ components/react/ui-primitives/tag-list/tag-list.tsx
@@ -4,19 +4,29 @@ import { Tag } from '@teambit/bad-jokes.ui-primitives.tag';
 import styles from './tag-list.module.scss';
 
 type TagListProps = {
+  /**
+   * onClick function when selecting a tag
+   */
   onPick?: (tag: string) => any;
+  /**
+   * content array to be presented inside the tags
+   */
   tags: string[];
+  /**
+   * override tag class
+   */
+  tagClass?: string;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 /**
  * renders an array of tags
  */
-export function TagList({ tags, className, onPick, ...rest }: TagListProps) {
+export function TagList({ tags, className, tagClass, onPick, ...rest }: TagListProps) {
   return (
     <div {...rest} className={classnames(className, styles.tagList)}>
       {tags &&
         tags.map((tag: string) => (
-          <Tag key={tag} onPick={onPick}>
+          <Tag className={tagClass} key={tag} onPick={onPick}>
             {tag}
           </Tag>
         ))}
