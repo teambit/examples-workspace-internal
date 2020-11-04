7cda072ee1561bcd2a1948a7ef551d137e4116f7
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2020-11-04 09:39:04 +0200

    Fix tag and tag-list

--- components/react/ui-primitives/tag/tag.composition.tsx
+++ components/react/ui-primitives/tag/tag.composition.tsx
@@ -7,3 +7,7 @@ export const LightTag = () => {
 export const DarkTag = () => {
   return <Tag style={{ backgroundColor: '#414141', color: 'white' }}>dark Tag</Tag>;
 };
+
+export const AlertTag = () => {
+  return <Tag onPick={() => alert('tag selected!')}>Alert Tag</Tag>;
+};
