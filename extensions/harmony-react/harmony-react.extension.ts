acb8ae2e65ddd846503dcff39798df9f6463e07a
Ran Mizrahi <ran@bit.dev>
2020-11-09 23:07:04 +0200

    added example for changing version of ts and jest

--- extensions/harmony-react/harmony-react.extension.ts
+++ extensions/harmony-react/harmony-react.extension.ts
@@ -1,18 +1,21 @@
 import { EnvsMain, EnvsAspect } from '@teambit/envs';
 import { ReactAspect, ReactMain } from '@teambit/react';
+import ts from 'typescript';
 
+const jest = require('jest');
 const tsconfig = require('./typescript/tsconfig.json');
 const webpackConfig = require('./webpack/webpack.config');
 
 export class HarmonyReactExtension {
   constructor(private react: ReactMain) {}
 
-  static dependencies: any = [EnvsAspect, ReactAspect];
+  static dependencies = [EnvsAspect, ReactAspect];
 
   static async provider([envs, react]: [EnvsMain, ReactMain]) {
     const harmonyReactEnv = react.compose([
-      // react.overrideTsConfig(tsconfig),
-      // react.overrideJestConfig(require.resolve('./jest/jest.config')),
+      // @ts-ignore
+      react.overrideTsConfig(tsconfig, ts),
+      react.overrideJestConfig(require.resolve('./jest/jest.config'), jest),
       // react.overrideDevServerConfig(webpackConfig),
     ]);
 
