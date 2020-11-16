72f1b7d1dee487804d2677b660f1e0bef96d9144
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2020-11-16 14:43:37 +0200

    config jest+enzyme for harmony-react

--- extensions/harmony-react/jest/jest.config.js
+++ extensions/harmony-react/jest/jest.config.js
@@ -2,4 +2,10 @@ const reactJestConfig = require('@teambit/react/jest/jest.config');
 
 module.exports = {
   ...reactJestConfig,
+
+  setupFilesAfterEnv: [require.resolve('./jest.setup.js')],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(prop-types|@teambit))",
+  ],
+  snapshotSerializers: ["enzyme-to-json/serializer"]
 };
