98104696e8df557c6e0e3e61bd97848273f83b98
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2020-12-13 20:12:20 +0200

    admin

--- components/react/ui-primitives/container/container.docs.tsx
+++ components/react/ui-primitives/container/container.docs.tsx
@@ -2,6 +2,6 @@ export default function Overview() {
     return null;
 };
 
-Overview.abstract = 'An imperfect container.';
+Overview.abstract = 'This is a customized version of the harmony scope container, using styled-jsx for its styling';
 
-Overview.labels = ['react', 'typescript', 'container', 'ui'];
+Overview.labels = ['react', 'typescript', 'container', 'ui', 'styledjsx', 'styled-jsx'];
